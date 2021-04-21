eeb5482887097f7c9797d36d768c460ab27115fe
yme5 <yme5@njit.edu>
2021-04-21 13:52:42 -0400

    Step 13:Set up view page

--- app/app.py
+++ app/app.py
@@ -1,44 +1,43 @@
 from typing import List, Dict
-import mysql.connector
 import simplejson as json
-from flask import Flask, Response
+from flask import Flask, request, Response, redirect
 from flask import render_template
-
+from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
 
 app = Flask(__name__)
+mysql = MySQL(cursorclass=DictCursor)
+
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'treesData'
+mysql.init_app(app)
 
 
-def trees_import() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'db',
-        'port': '3306',
-        'database': 'treesData'
-    }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor(dictionary=True)
-
-    cursor.execute('SELECT * FROM tblTreesImport')
-    result = cursor.fetchall()
-
-    cursor.close()
-    connection.close()
-
-    return result
-
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     user = {'username': 'Yousuf'}
-    trees_data = trees_import()
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblTreesImport')
+    result = cursor.fetchall()
+    return render_template('index.html', title='Home', user=user, trees=result)
 
-    return render_template('index.html', title='Home', user=user, trees=trees_data)
+@app.route('/view/<int:tree_id>', methods=['GET'])
+def record_view(tree_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblTreesImport WHERE id=%s', tree_id)
+    result = cursor.fetchall()
+    return render_template('view.html', title='View Form', tree=result[0])
 
-@app.route('/api/trees')
-def cities() -> str:
-    js = json.dumps(trees_import())
-    resp = Response(js, status=200, mimetype='application/json')
+@app.route('/api/v1/trees', methods=['GET'])
+def api_browse() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblTreesImport')
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 if __name__ == '__main__':
